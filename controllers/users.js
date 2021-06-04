c5805d57d9d7d16fd3a5797345fd146a70757aed
igor-skorokhodov <72555305+igor-skorokhodov@users.noreply.github.com>
2021-06-04 12:57:35 +0700

    3 iteration

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,4 @@
 const User = require("../models/user.js");
-const reg = /^(ftp|http|https):\/\/[^ "]+$/;
 
 function getUsers(req, res) {
   return User.find({})
@@ -42,9 +41,6 @@ function createUser(req, res) {
       res.status(200).send({ data: user });
     })
     .catch((err) => {
-      if (!reg.test(req.body.avatar)) {
-        res.status(400).send({ message: "Введены некорректные данные!" });
-      }
       if (err.name === "ValidationError") {
         res
           .status(400)
@@ -60,44 +56,57 @@ function createUser(req, res) {
 function updateUser(req, res) {
   const id = req.user._id;
 
-  return User.findByIdAndUpdate(id, {
-    name: req.body.name,
-    about: req.body.about,
-  })
+  return User.findByIdAndUpdate(
+    id,
+    {
+      name: req.body.name,
+      about: req.body.about,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
     .then((user) => {
-      if (
-        req.body.name.length < 2 ||
-        req.body.name.length > 30 ||
-        req.body.about.length < 2 ||
-        req.body.about.length > 30
-      ) {
-        res.status(400).send({ message: `Введены некорректные данные!` });
-      } else {
-        res.status(200).send({ data: user });
-      }
+      res.status(200).send({ data: user });
     })
     .catch((err) => {
-      res
-        .status(500)
-        .send({ message: `На сервере произошла ошибка ${err.name}!` });
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({ message: `Введены некорректные данные! Ошибка ${err.name}` });
+      } else {
+        res
+          .status(500)
+          .send({ message: `На сервере произошла ошибка ${err.name}!` });
+      }
     });
 }
 
 function updateAvatar(req, res) {
   const id = req.user._id;
 
-  return User.findByIdAndUpdate(id, { avatar: req.body.avatar })
+  return User.findByIdAndUpdate(
+    id,
+    { avatar: req.body.avatar },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
     .then((user) => {
-      if (!reg.test(req.body.avatar)) {
-        res.status(400).send({ message: "Введены некорректные данные!" });
-      } else {
-        res.status(200).send({ data: user });
-      }
+      res.status(200).send({ data: user });
     })
     .catch((err) => {
-      res
-        .status(500)
-        .send({ message: `На сервере произошла ошибка ${err.name}!` });
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({ message: `Введены некорректные данные! Ошибка ${err.name}` });
+      } else {
+        res
+          .status(500)
+          .send({ message: `На сервере произошла ошибка ${err.name}!` });
+      }
     });
 }
 
--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    requird: true,
+    required: true,
   },
 })
 
