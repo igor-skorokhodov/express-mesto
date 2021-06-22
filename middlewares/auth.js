091ebe23760ff1a8cb5b711ccb31662261a19cd7
igor-skorokhodov <72555305+igor-skorokhodov@users.noreply.github.com>
2021-06-22 17:44:02 +0700

    new

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация 1');
+    throw new AuthError('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new AuthError('Необходима авторизация 2');
+    throw new AuthError('Необходима авторизация');
   }
 
   req.user = payload;
