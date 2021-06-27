e050ea7332610a799e65f6a82a7af531f1687584
igor-skorokhodov <72555305+igor-skorokhodov@users.noreply.github.com>
2021-06-27 19:17:27 +0700

    new

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ userRoutes.patch('/users/me',
   celebrate({
     params: Joi.object()
       .keys({
-        userId: Joi.string().hex().length(24),
+        userId: Joi.string().length(24),
       })
       .unknown(true),
     body: Joi.object()
@@ -42,7 +42,7 @@ userRoutes.patch('/users/me/avatar',
   celebrate({
     params: Joi.object()
       .keys({
-        userId: Joi.string().hex().length(24),
+        userId: Joi.string().length(24),
       })
       .unknown(true),
     body: Joi.object()
